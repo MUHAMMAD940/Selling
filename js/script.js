555d5a62b118f4f2dbb9032e8fc5577cd0179f73
MUHAMMAD940 <muhammadraj@gmail.com>
2021-01-04 04:43:54 -0800

    Selling Project

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(function () {
+
+    // Slider part start
+
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+        });
+    });
+
+    // Menu fix js start
+
+    var navoff = $('.main_menu').offset().top;
+
+
+    $(window).on('scroll', function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navoff) {
+            $('.main_menu').addClass('menu_fix');
+        } else {
+            $('.main_menu').removeClass('menu_fix');
+        }
+
+    });
+
+
+    //smooth scrolling js Scroll donw 
+
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+});
